925cc225426dcb3f697c3466cffe1256648c9f33
Burak Kızılay <burak.kizilay@westerops.com>
2024-01-21 02:28:31 +0300

    base files created

---
+++ lib/pages/sample/view/sample_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SampleView extends StatelessWidget {
+  const SampleView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
